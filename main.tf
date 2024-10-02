0af0e3c018044ff43d11e2bed9de9297f7a6c81d
Jamie Jonas <jamie@oneten.tech>
2024-10-02 16:15:29 +0200

    3 another push

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "naming" {
 
 }
 resource "azuread_group" "main" {
-  for_each     = toset[var.default_groups]
+  for_each     = toset(var.default_groups)
   display_name = "module.naming.resource_group.name_unique-${each.key}"
   description  = "Default Groups Created By Terraform Module: module.naming.resource_group.name_unique-${each.key}"
 }
@@ -15,7 +15,7 @@ module "avm-res-resources-resourcegroup" {
   # insert the 2 required variables here
   location = var.location
   name     = module.naming.resource_group.name_unique
-  role_assignments = { for k, group in toset[var.default_groups] : k => {
+  role_assignments = { for k, group in toset(var.default_groups) : k => {
     principal_id         = azuread_group.main[each.key].object_id
     role_definition_name = each.key
     }
