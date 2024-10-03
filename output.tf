b28ac4a1cb49afad4a1d829ade218b5185ddf73d
Jamie Jonas <j.jonas@mmu.ac.uk>
2024-10-03 17:44:28 +0200

    added location

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "resource_group_name" {
   value = module.avm-res-resources-resourcegroup.name
   description = "The worklaods resource group name."
 }
+output "resource_group_location" {
+  value = module.avm-res-resources-resourcegroup.location
+  description = "The worklaods resource group location."
+}
 output "subnet" {
   value = {for k, subnet in azurerm_subnet.workloads : k => subnet.id}
   description = "Output the subnet ID and its key."
