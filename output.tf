2d0c9289fdb912c441c363762511bda49c9474b2
Jamie Jonas <j.jonas@mmu.ac.uk>
2024-10-03 17:32:41 +0200

    added subnets as output

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "resource_group_name" {
   value = module.avm-res-resources-resourcegroup.name
   description = "The worklaods resource group name."
 }
+output "subnet" {
+  value = {for k, subnet in azurerm_subnet.workloads : k => subnet.value.id}
+  description = "Output the subnet ID and its key."
+}
